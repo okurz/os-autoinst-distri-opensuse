df4a26d00bee6a3b2bad5c833b227b4652e26ff5
Oliver Kurz <okurz@suse.de>
2017-03-28 08:29:53 +0200

    Simplify yast2_dns_server test

--- tests/console/yast2_dns_server.pm
+++ tests/console/yast2_dns_server.pm
@@ -9,59 +9,42 @@
 # without any warranty.
 
 # Summary: New test for yast2-dns-server (service management)
+#   Test "yast2 dhcp-server" functionality.
+#   Ensure that all combinations of running/stopped and active/inactive
+#   can be set
 # Maintainer: mgriessmeier <mgriessmeier@suse.de>
 
 use base "console_yasttest";
 use strict;
 use testapi;
 
-# Test "yast2 dhcp-server" functionality
-# Ensure that all combinations of running/stopped and active/inactive
-# can be set
 
 # Assert if the dns service is running or stopped
 sub assert_running() {
-    my $self    = shift;
-    my $running = shift;
-
-    if ($running) {
-        assert_script_run 'systemctl is-active named | grep -E "^active"';
-    }
-    else {
-        assert_script_run 'systemctl is-active named | grep -E "^(inactive|unknown)"';
-    }
+    my ($running) = @_;
+    my $cmd = 'systemctl is-active named';
+    $cmd = '! ' . $cmd if $running;
+    assert_script_run $cmd;
 }
 
 # Assert if the dns service is enabled or disabled
 sub assert_enabled() {
-    my $self    = shift;
-    my $enabled = shift;
-
-    if ($enabled) {
-        assert_script_run 'systemctl is-enabled named | grep enabled';
-    }
-    else {
-        assert_script_run 'systemctl is-enabled named | grep disabled';
-    }
+    my ($enabled) = @_;
+    md $cmd = 'systemctl is-enabled named';
+    $cmd = '! ' . $cmd if $enabled;
+    assert_script_run $cmd;
 }
 
 sub run() {
-    my $self = shift;
-
-    #
-    # Preparation
-    #
     select_console 'root-console';
 
     # Make sure packages are installed
     assert_script_run 'zypper -n in yast2-dns-server bind SuSEfirewall2';
     # Let's pretend this is the first execution (could not be the case if
     # yast2_cmdline was executed before)
-    script_run 'rm /var/lib/YaST2/dns_server';
+    assert_script_run 'rm -f /var/lib/YaST2/dns_server';
 
-    #
     # First execution (wizard-like interface)
-    #
     script_run 'yast2 dns-server', 0;
     # Just do next-next until the last step
     assert_screen 'yast2-dns-server-step1';
@@ -70,59 +53,47 @@ sub run() {
     send_key 'alt-n';
     assert_screen 'yast2-dns-server-step3';
     # Enable dns server and finish
-    send_key 'alt-s';
-    send_key 'alt-f';
-    wait_idle;
+    wait_screen_change { send_key 'alt-s' };
+    wait_screen_change { send_key 'alt-f' };
     # The wizard-like interface still uses the old approach of always starting the service
     # while enabling it, so named should be both active and enabled
-    $self->assert_running(1);
-    $self->assert_enabled(1);
+    assert_running(1);
+    assert_enabled(1);
 
-    #
     # Second execution (tree-based interface)
-    #
     script_run 'yast2 dns-server', 0;
     assert_screen 'yast2-service-running-enabled';
     # Stop the service
     send_key 'alt-s';
     assert_screen 'yast2-service-stopped-enabled';
     # Cancel yast2 to check the effect
-    send_key 'alt-c';
-    if (check_screen('yast2-dns-server-quit')) {
-        send_key 'alt-y';
-    }
-    wait_idle;
-    $self->assert_running(0);
-    $self->assert_enabled(1);
+    wait_screen_change { send_key 'alt-c' };
+    send_key 'alt-y' if check_screen 'yast2-dns-server-quit', 0;
+    assert_running(0);
+    assert_enabled(1);
 
-    #
     # Third execution (tree-based interface)
-    #
     script_run 'yast2 dns-server', 0;
     assert_screen 'yast2-service-stopped-enabled';
     # Start the service
     send_key 'alt-s';
     assert_screen 'yast2-service-running-enabled';
     # Disable the service and finish
-    send_key 'alt-t';
-    send_key 'alt-o';
-    wait_idle;
-    $self->assert_running(1);
-    $self->assert_enabled(0);
+    wait_screen_change { send_key 'alt-t' };
+    wait_screen_change { send_key 'alt-o' };
+    assert_running(1);
+    assert_enabled(0);
 
-    #
     # Fourth execution (tree-based interface)
-    #
     script_run 'yast2 dns-server', 0;
     assert_screen 'yast2-service-running-disabled';
     # Stop the service
     send_key 'alt-s';
     assert_screen 'yast2-service-stopped-disabled';
     # Finish
-    send_key 'alt-o';
-    wait_idle;
-    $self->assert_running(0);
-    $self->assert_enabled(0);
+    wait_screen_change { send_key 'alt-o' };
+    assert_running(0);
+    assert_enabled(0);
 }
 
 1;
