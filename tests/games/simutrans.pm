781de1323d66235f1a2a71318a75533dbbceabb1
Oliver Kurz <okurz@suse.de>
2021-03-24 17:47:59 +0000

    commit

---
+++ tests/games/simutrans.pm
@@ -0,0 +1,24 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Startup and basics of the game simutrans
+# Maintainer: Oliver Kurz <okurz@suse.de>
+
+use base 'x11test';
+use strict;
+use warnings;
+use testapi;
+
+sub run {
+    select_console 'x11';
+    ensure_installed 'simutrans';
+    x11_start_program 'simutrans';
+}
+
+1;
